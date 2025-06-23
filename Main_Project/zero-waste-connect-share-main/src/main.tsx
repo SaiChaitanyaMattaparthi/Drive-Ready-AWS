f3afac6c5d5a9d92c3f64bf0fbfce0ae8940a2fe
M N G Sai Chaitanya <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2025-06-23 22:08:34 +0530

    main.tsx

--- Main_Project/zero-waste-connect-share-main/src/main.tsx
+++ Main_Project/zero-waste-connect-share-main/src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports); // ✅ Connect Amplify Auth + other services
 
 createRoot(document.getElementById("root")!).render(<App />);
